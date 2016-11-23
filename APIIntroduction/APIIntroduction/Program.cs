6ca90806bdccf51c3b0b62b1c2d90bf19ee49f59
Kyle <Kyle@KYLE-HP>
2016-11-22 20:58:26 -0600

    Adding initial code to hit the API

---
+++ APIIntroduction/APIIntroduction/Program.cs
@@ -0,0 +1,42 @@
+﻿using APIIntroduction.LeagueObjects;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string APIKeyFile = Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().CodeBase, "APIKey.txt");
+
+            if(!File.Exists(APIKeyFile))
+            {
+                Console.WriteLine("Please enter in your API Key");
+                Console.ReadLine();
+            }
+
+            string sURL = @"https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/143?api_key=<API-KEY>"; //Replace API-KEY with the actual API key
+
+            WebRequest wrGETURL = WebRequest.Create(sURL);
+
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+            using (StreamReader response = new StreamReader(wrGETURL.GetResponse().GetResponseStream()))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                MinimalChampionDto leagueChampion = (MinimalChampionDto)serializer.Deserialize(response, typeof(MinimalChampionDto));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
